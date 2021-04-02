13fba6a2a52f7dee6c20325ad97aadc2788fe1a1
keaton85 <>
2021-04-02 02:50:50 +0000

    added conversion functions, /api/convert route, created .env

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -1,26 +1,81 @@
 function ConvertHandler() {
   
   this.getNum = function(input) {
-    let result;
-    
+let regex=/[0-9.]+/
+    let result=input.match(regex)[0];
+    console.log(result)
     return result;
   };
   
   this.getUnit = function(input) {
-    let result;
+    let regex=/[a-zA-Z]+/
+    let result =input.match(regex)[0].toLowerCase()
     
+    console.log(result)
     return result;
   };
   
   this.getReturnUnit = function(initUnit) {
-    let result;
+   // let regex;
+let result
+
+switch(initUnit) {
+
+case "gal":
+result = "L";
+break;
+
+case "L":
+result = "gal"
+break;
+
+case "mi":
+result = "km";
+break;
+
+case "km":
+result ="mi"
+break;
+
+case "lbs":
+result = "kg";
+break;
+
+case "kg":
+result = "lbs"
+break;
+}
+
+  //  let result;
     
     return result;
   };
 
   this.spellOutUnit = function(unit) {
     let result;
-    
+
+  switch(unit) {
+  
+case "gal":
+result = "gallons";
+
+case "L":
+result = "liters"
+
+case "mi":
+result = "miles";
+
+case "km":
+result ="kilometers"
+
+case "lbs":
+result = "pounds";
+
+case "kg":
+result = "kilograms"
+
+}
+
     return result;
   };
   
@@ -34,7 +89,8 @@ function ConvertHandler() {
   };
   
   this.getString = function(initNum, initUnit, returnNum, returnUnit) {
-    let result;
+   // let unit = initUnit.spellOutUnit()
+    let result = `${initNum} ${initUnit} converts to ${returnNum} ${returnUnit}`
     
     return result;
   };
--- routes/api.js
+++ routes/api.js
@@ -6,5 +6,18 @@ const ConvertHandler = require('../controllers/convertHandler.js');
 module.exports = function (app) {
   
   let convertHandler = new ConvertHandler();
-
+  app.get('/api/convert', function(req, res){
+   console.log(req.query)
+  // return req.query
+   let inputNum = convertHandler.getNum(req.query.input);
+   let inputUnit = convertHandler.getUnit(req.query.input);
+   let returnUnit = convertHandler.getReturnUnit(inputUnit);
+   let returnNum = convertHandler.convert(inputNum,inputUnit);
+   let returnString = convertHandler.getString(inputNum,inputUnit,returnNum,returnUnit)
+  // let returnObject = convertHandler.convert(inputNum,inputUnit)
+   let outputObject = { initNum: inputNum,initUnit: inputUnit,returnNum: "returnObject",
+   returnUnit: returnUnit, string: returnString}
+ // return returnObject
+   res.json(outputObject)
+  })
 };
